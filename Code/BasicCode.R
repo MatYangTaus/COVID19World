4745ca604e4f5b836c7a86662ac7d6f4655aa0e6
MatYangTaus <ebikei@hotmail.com>
2020-04-19 23:06:15 -0700

    added

--- Code/BasicCode.R
+++ Code/BasicCode.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, skimr, lubridate)
+pacman::p_load(tidyverse, skimr, lubridate, ggthemes)
 
 #json_file <- "https://covidtracking.com/api/v1/states/current.json"
 #json_data <- fromJSON(paste(readLines(json_file), collapse=""))
@@ -26,13 +26,26 @@ state.list = df %>%
      filter(MaxCase > 5000) %>% 
      select(state)
 
-test = df %>% 
+df %>% 
      filter(state %in% state.list$state, positive > 500) %>% 
+     filter(state %in% c('CA', 'NJ', 'MA', 'NY', 'TX', 'FL')) %>% 
      group_by(state) %>% 
      mutate(day = c(1:n())) %>% 
      ungroup() %>% 
      ggplot(aes(x = day, y = log(positive), color = state)) +
           geom_point() +
           geom_line() +
-          coord_trans(y="log2") +
-          theme_minimal()
+          #coord_trans(y="log2") +
+          theme_fivethirtyeight()
+
+
+## World
+data = read.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+
+data2 = data %>% 
+     select(-c(Lat, Long)) %>% 
+     group_by(Country.Region, Province.State) %>% 
+     pivot_longer(-c(Country.Region, Province.State), names_to = "tempdate", values_to = "count") %>% 
+     mutate(n = c(1:n()), date = as.Date(ifelse(tempdate == 'X1.22.20', as.Date('2020-01-22'), as.Date('2020-01-22') +n -1))) %>% 
+     ungroup()
+
