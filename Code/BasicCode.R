7d3f07db55b70f45a28f5b016772a3656cced943
MatYangTaus <ebikei@hotmail.com>
2020-04-19 19:20:46 -0700

    added

--- Code/BasicCode.R
+++ Code/BasicCode.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, skimr)
+pacman::p_load(tidyverse, skimr, lubridate)
 
 #json_file <- "https://covidtracking.com/api/v1/states/current.json"
 #json_data <- fromJSON(paste(readLines(json_file), collapse=""))
@@ -6,6 +6,9 @@ pacman::p_load(tidyverse, skimr)
 
 #df = read.csv('https://covidtracking.com/api/v1/states/current.csv')
 df = read.csv('https://covidtracking.com/api/states/daily.csv') %>% 
-     arrange(state. date)
+     arrange(state, date) %>% 
+     select(-c(hash, dateChecked)) %>% 
+     mutate(fips = str_sub(paste0('0', fips), -2, -1), date = ymd(date))
 #df3 = read.csv('https://covidtracking.com/api/us/daily.csv')
 
+df.ca = filter(df, fips == '06')
