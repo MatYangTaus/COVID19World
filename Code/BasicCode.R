13c8f8a7655b932de017c69c0db28c54f7917002
MatYangTaus <ebikei@hotmail.com>
2020-04-21 08:41:00 -0700

    death added

--- Code/BasicCode.R
+++ Code/BasicCode.R
@@ -83,3 +83,48 @@ data2 %>%
           #coord_trans(y="log2") +
           theme_minimal()
      #     theme_fivethirtyeight()
+
+## World Death
+data = read.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+
+data2 = data %>% 
+        select(-c(Lat, Long)) %>% 
+        group_by(Country.Region, Province.State) %>% 
+        pivot_longer(-c(Country.Region, Province.State), names_to = "tempdate", values_to = "count") %>% 
+        mutate(n = c(1:n())) %>% #, date =  as.Date(ifelse(n == 1, '2020-01-22', NA))) %>% 
+        mutate(date = as.Date('2020-01-21') + n) %>% 
+        ungroup() %>% 
+        select(Country.Region, Province.State, date, count) %>% 
+        group_by(Country.Region, date) %>% 
+        summarize(Count = sum(count)) %>% 
+        ungroup()
+
+state.list2 = data2 %>% 
+        group_by(Country.Region) %>% 
+        mutate(MaxCase = max(Count)) %>% 
+        ungroup() %>% 
+        distinct(Country.Region, .keep_all = TRUE) %>% 
+        filter(MaxCase > 100) %>% 
+        select(Country.Region)
+
+data2 %>% 
+        group_by(Country.Region) %>% 
+        arrange(desc(date)) %>% 
+        slice(1) %>% 
+        filter(log(Count) < 6.25, log(Count) > 5)
+
+data2 %>% 
+        filter(Country.Region %in% state.list2$Country.Region, Count > 50) %>% 
+        #filter(Country.Region %in% c('Brazil', 'Japan', 'Singapore', 'Sweden', 'Korea, South', 'Canada', 'Philippines', 'Malaysia', 'Qatar')) %>% 
+        filter(Country.Region %in% c('Japan', 'Singapore', 'Pakistan', 'Korea, South', 'Dominican Republic','Israel', 'Norway')) %>% 
+        group_by(Country.Region) %>% 
+        mutate(day = c(1:n())) %>% 
+        ungroup() %>% 
+        ggplot(aes(x = day, y = log(Count), color = Country.Region)) +
+        geom_point() +
+        geom_line() +  
+        xlab('Days since case number exceeded 50') +
+        ylab('log Case') +
+        #coord_trans(y="log2") +
+        theme_minimal()
+#     theme_fivethirtyeight()
